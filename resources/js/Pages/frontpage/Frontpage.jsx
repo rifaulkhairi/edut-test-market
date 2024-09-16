8588d7479696e8e9c8017f89adae37299f65e133
rifaulkhairi <rifaulkhairi05@gmail.com>
2024-09-16 16:11:16 +0700

    perbaiki tampilan frontpage

--- resources/js/Pages/frontpage/Frontpage.jsx
+++ resources/js/Pages/frontpage/Frontpage.jsx
@@ -21,40 +21,40 @@ export default function Frontpage({ auth, produks, base_url, cart }) {
                         <Hero></Hero>
                     </div>
                     <div className="flex w-full max-w-5xl mt-2 pb-2">
-                        <div className="flex w-full h-24 items-center justify-evenly flex-row gap-5">
+                        <div className="flex w-full h-24 items-center justify-center flex-row gap-5">
                             <button
-                                className="flex w-20 items-center justify-center  h-20  border-[1px]  flex-col rounded-3xl hover:-translate-y-1 transition-all"
+                                className="flex w-20 items-center justify-center  h-20 bg-[#008a60]  flex-col rounded-3xl hover:-translate-y-1 transition-all"
                                 variant="text"
                                 onClick={(e) => {
                                     e.preventDefault();
                                     router.visit("/user/paketsoal");
                                 }}
                             >
-                                <ClipboardDocumentIcon className="h-10 w-10 text-[#daa21c]"></ClipboardDocumentIcon>
-                                <p className="capitalize font-semibold text-xs text-[#daa21c]">
+                                <ClipboardDocumentIcon className="h-10 w-10 text-white"></ClipboardDocumentIcon>
+                                <p className="capitalize font-semibold text-xs text-white">
                                     Soal ku
                                 </p>
                             </button>
 
                             <button
-                                className="flex  h-20 w-20 justify-center border-[1px]  flex-col gap-y-2 items-center rounded-3xl hover:-translate-y-1 transition-all"
+                                className="flex  h-20 w-20 bg-[#7464a7] justify-center  flex-col gap-y-2 items-center rounded-3xl hover:-translate-y-1 transition-all"
                                 variant="text"
                             >
-                                <HeartIcon className="h-8 w-8 text-[#7464a7]"></HeartIcon>
-                                <p className="capitalize font-semibold text-xs text-center text-[#7464a7]">
+                                <HeartIcon className="h-8 w-8 text-white"></HeartIcon>
+                                <p className="capitalize font-semibold text-xs text-center text-white">
                                     Favorit
                                 </p>
                             </button>
                             <button
-                                className="flex w-20 h-20 justify-center border-[1px] flex-col gap-y-2 items-center rounded-3xl hover:-translate-y-1 transition-all"
+                                className="flex w-20 h-20 bg-[#00c29d] justify-center border-[1px] flex-col gap-y-2 items-center rounded-3xl hover:-translate-y-1 transition-all"
                                 variant="text"
                                 onClick={(e) => {
                                     e.preventDefault();
                                     router.visit("/riwayattransaksi");
                                 }}
                             >
-                                <WalletIcon className="h-8 w-8 text-[#00c29d]"></WalletIcon>
-                                <p className="capitalize font-normal text-xs text-center text-[#00c29d]">
+                                <WalletIcon className="h-8 w-8 text-white font-bold"></WalletIcon>
+                                <p className="capitalize font-normal text-xs text-center text-white">
                                     Riwayat
                                 </p>
                             </button>
@@ -65,10 +65,8 @@ export default function Frontpage({ auth, produks, base_url, cart }) {
 
                 {/* Produk */}
                 <section className="flex w-full h-full items-center justify-center flex-col gap-2">
-                    <div className="flex w-full h-12 bg-white max-w-5xl items-center border-b-2 border-[#daa21c]">
-                        <div className="m-auto font-bold text-[#daa21c]">
-                            Rekomendasi
-                        </div>
+                    <div className="flex w-full h-12 bg-white max-w-5xl items-center border-b-2 border-gray-600">
+                        <div className="m-auto text-gray-600">Rekomendasi</div>
                     </div>
 
                     <div className="w-full max-w-5xl flex flex-wrap min-w-5xl gap-2">
